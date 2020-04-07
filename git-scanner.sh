ec3277796dfdefe8ecbcba7ebaea3f90ccb372cc
Matt Noakes <mattnoakes@me.com>
2020-04-07 17:53:56 +0100

    Switch from ls-remote to remote get-url

--- git-scanner.sh
+++ git-scanner.sh
@@ -10,11 +10,11 @@ cd ~
 # Print the repo name, check for existence of remotes then print the URI if it exits
 processor=$(cat <<- CMD
     echo "\033[94m\${0:2:\${#0}-7}\033[0m"
-    output=\$(git --git-dir="\${0:2:\${#0}-7}/.git" remote -v)
+    output=\$(git --git-dir="\${0:2:\${#0}-7}/.git" remote)
     printf "    "
     if ((\${#output} > 0))
     then
-        git --git-dir="\${0:2:\${#0}-7}/.git" ls-remote --get-url
+        git --git-dir="\${0:2:\${#0}-7}/.git" remote get-url \$output
     else
         echo "\033[90mNo remotes\033[0m"
     fi
