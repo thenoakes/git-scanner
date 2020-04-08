459d16189ee53bc61a66ba76e5dbd0bbac742e09
Matt Noakes <mattnoakes@me.com>
2020-04-08 22:49:01 +0100

    Add printing out of pending changes

--- git-scanner.sh
+++ git-scanner.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-source ./mnopts/mnopts.sh "ignore:i" "" "$@"
+source ./mnopts/mnopts.sh "ignore-none:i show-pending:s" "" "$@"
 
 [[ "$opt_i" = "true" ]] && echo "Ignoring repositories with no remote specified (-i option)"
+[[ "$opt_s" = "true" ]] && echo "Showing pending changes where relevant (-s option)"
 
 # Get the working directory as for storing the list
 pwd=$(pwd)
@@ -13,18 +14,25 @@ cd ~
 # Construct the command for processing and adding each repo to the list:
 # Print the repo name, check for existence of remotes then print the URI if it exits
 processor=$(cat <<- CMD
-    output=\$(git --git-dir="\${0:2:\${#0}-7}/.git" remote)
+    repoPath="\${0:2:\${#0}-7}"
+    output=\$(git --git-dir="\${repoPath}/.git" remote)
     if ((\${#output} > 0))
     then
-        echo "\033[94m\${0:2:\${#0}-7}\033[0m"
+        echo "\033[94m\${repoPath}\033[0m"
         for remote in \$output
         do
             printf "    \$remote: "
-            git --git-dir="\${0:2:\${#0}-7}/.git" remote get-url \$remote
+            git --git-dir="\${repoPath}/.git" remote get-url \$remote
         done
+        status="\$(git -c color.status=always --git-dir="\${repoPath}/.git" --work-tree="\${repoPath}" status -s)"
+        if ((\${#status} > 0)) && [ "$opt_s" == "true" ]
+        then
+            echo "    ------ PENDING CHANGES ------"
+            echo "\$status" | sed 's/^/    /'
+        fi
     elif [ "$opt_i" != "true" ]
     then
-        echo "\033[94m\${0:2:\${#0}-7}\033[0m"
+        echo "\033[94m\${repoPath}\033[0m"
         echo "    \033[90mNo remotes\033[0m"
     fi
 CMD
