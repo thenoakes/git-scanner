098ee09f9027477394c05ffa6823b2e89ee12a04
Matt Noakes <mattnoakes@me.com>
2021-04-25 16:14:56 +0100

    Split out some functions

--- git-scanner.go
+++ git-scanner.go
@@ -9,14 +9,32 @@ import (
 )
 
 func main() {
-	blueColour := "\033[94m"
-	greyColour := "\033[90m"
-	defaultColour := "\033[0m"
+	const (
+		blueColour    = "\033[94m"
+		greyColour    = "\033[90m"
+		defaultColour = "\033[0m"
+	)
 
-	homeDir, _ := os.UserHomeDir()
+	homeDir, err := os.UserHomeDir()
+
+	// isHiddenRoot returns true for a hidden directory which is a direct descendent of ~
+	isHiddenRoot := func(path string, info os.FileInfo) bool {
+		return path == filepath.Join(homeDir, info.Name()) && strings.HasPrefix(info.Name(), ".")
+	}
+
+	getRepoName := func(path string) string {
+		shortPath := strings.Replace(path, homeDir, "~", 1)
+		// Strip the ~ prefix and .git suffix and print to console
+		return shortPath[2 : len(shortPath)-5]
+	}
+
+	if err != nil {
+		fmt.Println("Fatal error: unable to determine home directory")
+		os.Exit(1)
+	}
 
 	// Walk all filepaths from the user's home directory...
-	err := filepath.Walk(homeDir,
+	err = filepath.Walk(homeDir,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				//return err
@@ -26,40 +44,31 @@ func main() {
 			}
 
 			// Skip hidden directories which are direct descendents
-			if info.IsDir() && path == filepath.Join(homeDir, info.Name()) && strings.HasPrefix(info.Name(), ".") {
+			if info.IsDir() && isHiddenRoot(path, info) {
 				return filepath.SkipDir
 			}
 
 			// Log all .git directories
 			if info.IsDir() && info.Name() == ".git" {
-				// Get a path relative to ~
-				shortPath := strings.Replace(path, homeDir, "~", 1)
-
-				// Strip the ~ prefix and .git suffix and print to console
-				repoName := shortPath[2 : len(shortPath)-5]
-				fmt.Print(blueColour, repoName, defaultColour, "\n")
+				// Print the path relative to ~
+				fmt.Print(blueColour, getRepoName(path), defaultColour, "\n")
 
 				// Get the git remotes by name if there are any
 				gitCmd := exec.Command("git", "--git-dir="+path, "remote")
 				result, _ := gitCmd.Output()
 
 				// Parse the results
-				remoteOutput := strings.Split(strings.TrimSpace(string(result)), "\n")
+				remoteOutput := strings.Split(cleanse(result), "\n")
 
 				// Filter out empty lines
-				remotes := []string{}
-				for _, line := range remoteOutput {
-					if len(line) > 0 {
-						remotes = append(remotes, line)
-					}
-				}
+				remotes := cleanseLines(remoteOutput)
 
 				// Obtain URLs and print alongside remote names
 				if len(remotes) > 0 {
 					for _, remote := range remotes {
 						remoteCmd := exec.Command("git", "--git-dir="+path, "remote", "get-url", remote)
 						remoteUrl, _ := remoteCmd.Output()
-						fmt.Printf("    %s: %s\n", remote, strings.TrimSpace(string(remoteUrl)))
+						fmt.Printf("    %s: %s\n", remote, cleanse(remoteUrl))
 					}
 				} else {
 					fmt.Print(greyColour, "    No remotes", defaultColour, "\n")
@@ -76,6 +85,18 @@ func main() {
 		os.Exit(0)
 	}
 
-	//fmt.Printf("%d", len(repos))
+}
 
+func cleanse(output []byte) string {
+	return strings.TrimSpace(string(output))
+}
+
+func cleanseLines(output []string) []string {
+	results := []string{}
+	for _, line := range output {
+		if len(line) > 0 {
+			results = append(results, line)
+		}
+	}
+	return results
 }
